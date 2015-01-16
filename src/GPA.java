9f7016b9ac1d6e73fb5d569a22416d1296c9e376
Nimit Kalra <nimit@nimitkalra.com>
2015-01-16 16:19:42 -0600

    More intro sequence work.

--- src/GPA.java
+++ src/GPA.java
@@ -8,7 +8,21 @@ public class GPA
 		System.out.println("Welcome to GPA. Let's calculate your GPA.");
 		System.out.println("=========================================\n\n");
 
-		System.out.print("Firstly, we'll build your portfolio.");
-		Thread.sleep(1000);
+		System.out.print("Firstly, we'll build your portfolio of classes.");
+		try {
+			Thread.sleep(500);
+			System.out.print(".");
+
+			Thread.sleep(500);
+			System.out.print(".");
+
+			Thread.sleep(200);
+			System.out.print("\n-------------------------------------------------\n\n");
+		} catch(InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+
+		System.out.print("How many high school level classes have you taken so far?");
+		Scanner keyboard = new Scanner(System.in);
 	}
 }
