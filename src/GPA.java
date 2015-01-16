228bc0b71e40ed4c6470cc05776a04b2fe2a36a0
Nimit Kalra <nimit@nimitkalra.com>
2015-01-16 16:14:50 -0600

    Working on intro sequence.

--- src/GPA.java
+++ src/GPA.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class GPA
 {
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to GPA. Let's calculate your GPA.\n\n");
+		System.out.println("Welcome to GPA. Let's calculate your GPA.");
+		System.out.println("=========================================\n\n");
 
-		System.out.print("");
+		System.out.print("Firstly, we'll build your portfolio.");
+		Thread.sleep(1000);
 	}
 }
