4c19f9141b2567699b4149a306f51c64f454d6e8
Nimit Kalra <nimit@nimitkalra.com>
2015-01-16 16:50:32 -0600

    ...and fixed it.

--- src/Number.java
+++ src/Number.java
@@ -2,20 +2,24 @@ public class Number
 {
 	public static String getEnding(int number)
 	{
-		if (number >= 4 && number <= 20) return "th";
+		String ending;
+
+		if (number >= 4 && number <= 20) ending = "th";
 
 		int last_digit = number % 10;
 		switch (last_digit) {
-			case 0: return "th";
+			case 0: ending = "th";
 					break;
-			case 1: return "st";
+			case 1: ending = "st";
 					break;
-			case 2: return "nd";
+			case 2: ending = "nd";
 					break;
-			case 3: return "rd";
+			case 3: ending = "rd";
 					break;
-			default: return "th";
+			default: ending = "th";
 					 break;
 		}
+
+		return ending;
 	}
 }
