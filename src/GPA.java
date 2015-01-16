c2fb819f579b01272b224cd89728490335312320
Nimit Kalra <nimit@nimitkalra.com>
2015-01-16 16:08:28 -0600

    Began intro sequence.

---
+++ src/GPA.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class GPA
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to GPA. Let's calculate your GPA.\n\n");
+
+		System.out.print("");
+	}
+}
