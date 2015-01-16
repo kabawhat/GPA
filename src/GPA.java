e8f2c7ab57d554761048bfb75665eb868009aedd
Nimit Kalra <nimit@nimitkalra.com>
2015-01-16 16:25:37 -0600

    Checks for existing "user" now.

--- src/GPA.java
+++ src/GPA.java
@@ -5,24 +5,36 @@ public class GPA
 {
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to GPA. Let's calculate your GPA.");
-		System.out.println("=========================================\n\n");
+		File portfolio = new File("portfolio.dat");
 
-		System.out.print("Firstly, we'll build your portfolio of classes.");
-		try {
-			Thread.sleep(500);
-			System.out.print(".");
+		if (portfolio.length() == 0) { // New "user".
+			System.out.println("Welcome to GPA. Let's calculate your GPA.");
+			System.out.println("=========================================\n\n");
 
-			Thread.sleep(500);
-			System.out.print(".");
+			System.out.print("Firstly, we'll build your portfolio of classes.");
+			try {
+				Thread.sleep(500);
+				System.out.print(".");
 
-			Thread.sleep(200);
-			System.out.print("\n-------------------------------------------------\n\n");
-		} catch(InterruptedException ex) {
-			Thread.currentThread().interrupt();
+				Thread.sleep(500);
+				System.out.print(".");
+
+				Thread.sleep(200);
+				System.out.print("\n-------------------------------------------------\n\n");
+			} catch(InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+
+			System.out.print("How many high school level classes have you taken so far? ");
+			Scanner keyboard = new Scanner(System.in);
+
+			int number_of_classes = keyboard.nextInt();
+			for (int i = 1; i <= number_of_classes; i++) {
+				System.out.println("What was this class called? ");
+
+			}
+		} else { // Existing "user" with a portfolio.
+
 		}
-
-		System.out.print("How many high school level classes have you taken so far?");
-		Scanner keyboard = new Scanner(System.in);
 	}
 }
