55b0f25192a64b0866b06b5af4e458b1f7c6f947
Nimit Kalra <nimit@nimitkalra.com>
2015-01-16 18:18:56 -0600

    Single course mode.

--- src/GPA.java
+++ src/GPA.java
@@ -48,73 +48,55 @@ public class GPA
 
 	public static void main(String args[])
 	{
-		File portfolio = new File("portfolio.dat");
+		System.out.println("Welcome to GPA. Let's calculate your GPA.");
+		System.out.println("=========================================\n\n");
 
-		if (portfolio.length() == 0) { // New "user".
-			System.out.println("Welcome to GPA. Let's calculate your GPA.");
-			System.out.println("=========================================\n\n");
+		int courseCount = 1;
+		ArrayList<Course> courses = new ArrayList<Course>();
+		ArrayList<Double> firstSemesterSixWeekAverages = new ArrayList<Double>();
+		ArrayList<Double> secondSemesterSixWeekAverages = new ArrayList<Double>();
 
-			System.out.print("Firstly, we'll build your portfolio of classes...");
-			System.out.print("\n-------------------------------------------------\n\n");
+		for (int i = 1; i <= courseCount; i++) {
+			Course course = new Course();
 
-			System.out.print("How many high school level courses have you taken so far? ");
-			Scanner keyboard = new Scanner(System.in);
+			System.out.print("What is this course called? ");
+			keyboard.nextLine(); // Picks up inevitable newline character (\n).
+			String courseName = keyboard.nextLine();
+			course.setName(courseName);
 
-			int courseCount = keyboard.nextInt();
-			ArrayList<Course> courses = new ArrayList<Course>();
-			ArrayList<Double> firstSemesterSixWeekAverages = new ArrayList<Double>();
-			ArrayList<Double> secondSemesterSixWeekAverages = new ArrayList<Double>();
+			System.out.print("Course level? (AP, IB, GT, Pre-AP, On Level) ");
+			String courseLevel = keyboard.nextLine();
+			course.setLevel(courseLevel);
 
-			for (int i = 1; i <= courseCount; i++) {
-				if (i == 1) {
-					System.out.println("\nLet's start with course #" + i + ".");
-				} else {
-					System.out.println("\nNow onto course #" + i + ".");
+			for (int j = 1; j <= 6; j++) {
+				if (j == 4) {
+					System.out.print("Midterm? ");
+					double midterm = keyboard.nextDouble();
+
+					course.setMidterm(midterm);
 				}
 
-				Course course = new Course();
+				System.out.print(j + Number.getEnding(j) + " 6 weeks average? ");
+				double courseSixWeeksAverage = keyboard.nextDouble();
 
-				System.out.print("What is this course called? ");
-				keyboard.nextLine(); // Picks up inevitable newline character (\n).
-				String courseName = keyboard.nextLine();
-				course.setName(courseName);
-
-				System.out.print("Course level? (AP, IB, GT, Pre-AP, On Level) ");
-				String courseLevel = keyboard.nextLine();
-				course.setLevel(courseLevel);
-
-				for (int j = 1; j <= 6; j++) {
-					if (j == 4) {
-						System.out.print("Midterm? ");
-						double midterm = keyboard.nextDouble();
-
-						course.setMidterm(midterm);
-					}
-
-					System.out.print(j + Number.getEnding(j) + " 6 weeks average? ");
-					double courseSixWeeksAverage = keyboard.nextDouble();
-
-					if (j == 1 || j == 2 || j == 3) {
-						firstSemesterSixWeekAverages.add(courseSixWeeksAverage);
-					}
-
-					if (j == 4 || j == 5 || j == 6) {
-						secondSemesterSixWeekAverages.add(courseSixWeeksAverage);
-					}
+				if (j == 1 || j == 2 || j == 3) {
+					firstSemesterSixWeekAverages.add(courseSixWeeksAverage);
 				}
 
-				System.out.print("Final? ");
-				double finalExam = keyboard.nextDouble();
-
-				course.setFinal(finalExam);
-
-				course.setFirstSemesterSixWeeksAverages(firstSemesterSixWeekAverages);
-				course.setSecondSemesterSixWeekAverages(secondSemesterSixWeekAverages);
-
-				System.out.println("-\n" + course + "\n\n");
+				if (j == 4 || j == 5 || j == 6) {
+					secondSemesterSixWeekAverages.add(courseSixWeeksAverage);
+				}
 			}
-		} else { // Existing "user" with a portfolio.
 
+			System.out.print("Final? ");
+			double finalExam = keyboard.nextDouble();
+
+			course.setFinal(finalExam);
+
+			course.setFirstSemesterSixWeeksAverages(firstSemesterSixWeekAverages);
+			course.setSecondSemesterSixWeekAverages(secondSemesterSixWeekAverages);
+
+			System.out.println("-\n" + course + "\n\n");
 		}
 	}
 }
